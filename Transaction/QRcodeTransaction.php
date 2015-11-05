ab5ba95b10f3dfbc9c9ce527576ae4bcc8e8f2ce
Joyd Sousa <jsousa@inter-invest.fr>
2015-11-05 11:43:07 +0100

    Nouvelle modif sur les QRcode

--- Transaction/QRcodeTransaction.php
+++ Transaction/QRcodeTransaction.php
@@ -11,13 +11,15 @@ class QRcodeTransaction extends Transaction implements TransactionInterface
     {
         $qrcode = $this->options['qrcode'];
         $y = isset($this->options['y']) ? $this->options['y'] : '';
+        $x = isset($this->options['x']) ? $this->options['x'] : '';
+        $w = isset($this->options['w']) ? $this->options['w'] : 15;
+        $h = isset($this->options['h']) ? $this->options['h'] : 15;
         $textColor = isset($this->options['textColor']) ? $this->options['textColor'] : array(0, 0, 0);
 
         $this
-            ->setOption('break', false)
-            ->add('qRcode', array('qrcode' => $qrcode, 'y' => $y, 'h' => 15, 'w' => 15))
+            ->add('qRcode', array('qrcode' => $qrcode, 'y' => $y, 'x' => $x, 'h' => $h, 'w' => $w))
             ->add('textOptions', array('color' => $textColor, 'size' => 6))
-            ->add('html', array('html' => $qrcode, 'y' => $y+15))
+            ->add('html', array('html' => $qrcode, 'y' => $y + $h, 'x' => $x))
         ;
     }
 }
